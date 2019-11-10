b82c554db2c72dd5c0a92c685f2cd304e3550698
MFHava <mfh@live.at>
2019-11-10 23:26:16 +0100

    added empty files for tests

---
+++ test/atomic_forward_list.cpp
@@ -0,0 +1,14 @@
+
+//          Copyright Michael Florian Hava 2012.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file ../../LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#include <boost/test/unit_test.hpp>
+#include "psx/atomic_forward_list.hpp"
+
+BOOST_AUTO_TEST_SUITE(atomic_forward_list)
+
+//TODO: BOOST_AUTO_TEST_CASE(<name>) {}
+
+BOOST_AUTO_TEST_SUITE_END()
