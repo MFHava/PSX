3f11326a4a104045c72ee43ba9d59e186bbc0364
MFHava <mfh@live.at>
2019-11-11 01:07:54 +0100

    supporting custom constructions

--- inc/psx/tls.hpp
+++ inc/psx/tls.hpp
@@ -10,10 +10,15 @@
 #include "atomic_forward_list.hpp"
 
 namespace psx {
+	//! @brief temporary thread-local storage
+	//! @tparam Type type of thread-local storage
+	//! @tparam Allocator allocator to use when allocating the thread-local storage
 	template<typename Type, typename Allocator = std::allocator<Type>>
 	class tls final {
+		static_assert(std::is_copy_constructible_v<Type>);
+
 		using node_type = std::pair<std::thread::id, Type>;
-		using list_type = atomic_forward_list<node_type>;
+		using list_type = atomic_forward_list<node_type, typename std::allocator_traits<Allocator>::template rebind_alloc<node_type>>;
 
 		list_type list;
 		const std::function<Type()> init;
@@ -35,8 +40,6 @@ namespace psx {
 
 			iterator_t() noexcept =default;
 
-			operator iterator_t<true>() const noexcept { return it; }
-
 			auto operator++() noexcept -> iterator_t & {
 				++it;
 				return *this;
@@ -56,17 +59,10 @@ namespace psx {
 			auto operator!=(const iterator_t & lhs, const iterator_t & rhs) noexcept { return !(lhs == rhs); }
 		};
 	public:
-		using iterator       = iterator_t<false>;
-		using const_iterator = iterator_t<true>;
-
-		template<typename T = Type, typename = std::enable_if_t<std::is_default_constructible_v<T>>>
-		tls() : init{[] { return Type{}; }} {}
-
-		template<typename T = Type, typename = std::enable_if_t<std::is_copy_constructible_v<T>>>
-		tls(const Type & value) : init{[value] { return value; }} {}
-
-		template<typename T = Type, typename = std::enable_if_t<std::is_copy_constructible_v<T> && std::is_move_constructible_v<T>>>
-		tls(Type && value) : init{[value{std::move(value)}] { return value; }} {}
+		template<typename... Args, typename = std::enable_if_t<std::is_constructible_v<Type, Args...>>>
+		tls(
+			Args &&... args //!< [in] params to initialize first instance of thread-local storage
+		) : init{[value{Type{std::forward<Args>(args)...}}] { return value; }} {}
 
 		tls(const tls &) =delete;
 		tls(tls &&) noexcept =delete;
@@ -74,19 +70,30 @@ namespace psx {
 		auto operator=(tls &&) noexcept -> tls & =delete;
 		~tls() noexcept =default;
 
+		//! @brief get access to thread-local storage
+		//! @throws ??? any exception thrown by allocator for/copy constructor of thread-local storage
+		//! @note allocates thread-local storage on first call from thread
+		[[nodiscard]]
 		auto local() -> Type & {
 			const auto id{std::this_thread::get_id()};
 			if(const auto it{std::find_if(std::begin(list), std::end(list), [&](const auto & p) { return p.first == id; })}; it != std::end(list)) return it->second;
 			return list.emplace_front(id, init()).second;
 		}
 
+		//! @brief clears all thread-local storage
+		//! @attention never invoke from concurrent context!
 		void clear() noexcept { list.clear(); }
 
-		auto begin() const noexcept { return const_iterator{std::begin(list)}; }
-		auto begin()       noexcept { return iterator{std::begin(list)}; }
-		auto end() const noexcept { return const_iterator{std::end(list)}; }
-		auto end()       noexcept { return iterator{std::end(list)}; }
+		//! @name Iteration
+		//! @brief forward iteration support for thread-local storage
+		//! @attention don't invoke concurrently with calls to @ref local
+		//! @{
+		using const_iterator = iterator_t<true>;
+		using iterator       = iterator_t<false>;
+		auto begin() const noexcept -> const_iterator { return std::begin(list); }
+		auto begin()       noexcept ->       iterator { return std::begin(list); }
+		auto end() const noexcept -> const_iterator { return std::end(list); }
+		auto end()       noexcept ->       iterator { return std::end(list); }
+		//! @}
 	};
-
 }
-
